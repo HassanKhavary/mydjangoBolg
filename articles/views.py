f4e896d7d9dd68026ba0d5dae4d1f5a6797d2dc7
HassanKhavary <hassan.khavary1375@gmail.com>
2021-11-29 18:11:52 +0330

    create project

---
+++ articles/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from . import models
+
+def articles_list(request):
+    articles=models.Articles.objects.all().order_by('date')
+    args={'articles':articles}
+    return render(request, 'articles/articles.html',args)
